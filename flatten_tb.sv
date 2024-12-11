9a6394a867c4a4a3c8d80889e5836cc986618a08
silasejohn <sejohn@usc.edu>
2024-12-11 01:27:14 -0800

    flatten works for 3d input now

--- flatten_tb.sv
+++ flatten_tb.sv
@@ -2,20 +2,22 @@ module flatten_tb;
 
     // init parameters
     parameter int WIDTH = 16;                                    // Bit width of input
+    parameter int CHANNELS = 2;                                    // # of channels
     parameter int DIM3_WIDTH = 4;                              // WIDTH of 3D Matrix (Reduced for testing)
     parameter int DIM3_HEIGHT = 4;                             // HEIGHT of 3D Matrix
-    parameter int DIM1_LENGTH = DIM3_WIDTH * DIM3_HEIGHT; // WIDTH of output feature map
+    parameter int DIM1_LENGTH = CHANNELS * DIM3_WIDTH * DIM3_HEIGHT; // WIDTH of output feature map
 
     // init signals
     logic clk;
-    logic signed [WIDTH-1:0] input_3D_maxpool_matrix [0:DIM3_HEIGHT-1][0:DIM3_WIDTH-1];
+    logic signed [WIDTH-1:0] input_3D_maxpool_matrix [0:CHANNELS-1][0:DIM3_HEIGHT-1][0:DIM3_WIDTH-1];
     logic signed [WIDTH-1:0] input_1D_fcl_matrix [0:DIM1_LENGTH-1];
-    logic signed [WIDTH-1:0] output_3D_maxpool_matrix [0:DIM3_HEIGHT-1][0:DIM3_WIDTH-1];
+    logic signed [WIDTH-1:0] output_3D_maxpool_matrix [0:CHANNELS-1][0:DIM3_HEIGHT-1][0:DIM3_WIDTH-1];
     logic signed [WIDTH-1:0] output_1D_fcl_matrix [0:DIM1_LENGTH-1];
 
     // uut the flatten module
     flatten #(
         .WIDTH(WIDTH),
+        .CHANNELS(CHANNELS),
         .DIM3_WIDTH(DIM3_WIDTH),
         .DIM3_HEIGHT(DIM3_HEIGHT),
         .DIM1_LENGTH(DIM1_LENGTH)
@@ -32,9 +34,11 @@ module flatten_tb;
 
     // setup 3D matrix
     task initialize_3D_matrix();
-        for (int row = 0; row < DIM3_HEIGHT; row++) begin
-            for (int col = 0; col < DIM3_WIDTH; col++) begin
-                input_3D_maxpool_matrix[row][col] = (row * DIM3_WIDTH + col) + 1; // sets to basic incrementing values
+        for (int ch = 0; ch < CHANNELS; ch++) begin
+            for (int row = 0; row < DIM3_HEIGHT; row++) begin
+                for (int col = 0; col < DIM3_WIDTH; col++) begin
+                    input_3D_maxpool_matrix[ch][row][col] = (ch * DIM3_WIDTH * DIM3_HEIGHT) + (row * DIM3_WIDTH * col) + 1; // sets to basic incrementing values
+                end
             end
         end
     endtask
@@ -47,11 +51,15 @@ module flatten_tb;
     endtask
 
     // pretty print functions //
-    task display_3D_matrix(logic signed [WIDTH-1:0] matrix [0:DIM3_HEIGHT-1][0:DIM3_WIDTH-1]);
+    task display_3D_matrix(logic signed [WIDTH-1:0] matrix [0:CHANNELS-1][0:DIM3_HEIGHT-1][0:DIM3_WIDTH-1]);
         $display("3D Matrix:");
-        for (int row = 0; row < DIM3_HEIGHT; row++) begin
-            for (int col = 0; col < DIM3_WIDTH; col++) begin
-                $write("%0d ", matrix[row][col]); // pretty print
+        for (int ch = 0; ch < CHANNELS; ch++) begin
+            $display("Channel %0d:", ch);
+            for (int row = 0; row < DIM3_HEIGHT; row++) begin
+                for (int col = 0; col < DIM3_WIDTH; col++) begin
+                    $write("%0d ", matrix[ch][row][col]); // pretty print
+                end
+                $display();
             end
             $display();
         end
