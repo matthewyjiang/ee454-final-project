af70d248956fa614899f57b0704f2b06ff1a7b7f
Matthew Jiang <jiangmy@usc.edu>
2024-12-10 19:41:51 -0800

    refactor cnn_top and fully_connected_layer for improved weight handling and state management

--- cnn_top.sv
+++ cnn_top.sv
@@ -3,17 +3,15 @@ module cnn_top
     parameter int WIDTH = 16, 
     parameter int LEARNING_RATE,
     parameter int FCL_INPUT_DIM = 4,
-    parameter int FCL_OUTPUT_DIM = 10,
+    parameter int FCL_OUTPUT_DIM = 10
 ) 
-
 (
     input  logic clk,
-    input  logic reset,
+    input  logic reset
 );
 
-
-signed logic [WIDTH-1:0] fcl_input_weights [FCL_OUTPUT_DIM][FCL_INPUT_DIM];
-signed logic [WIDTH-1:0] fcl_output_weights [FCL_OUTPUT_DIM][FCL_INPUT_DIM];
+signed logic [WIDTH-1:0] fcl_input_weights [FCL_INPUT_DIM+1][FCL_OUTPUT_DIM];
+signed logic [WIDTH-1:0] fcl_output_weights [FCL_INPUT_DIM+1][FCL_OUTPUT_DIM];
 
 module fully_connected_layer 
     #(
@@ -33,43 +31,39 @@ module fully_connected_layer
         .output_weights(fcl_output_weights)
     );
 
-logic [WIDTH*OUTPUT_DIM-1:0] lfsr_out;
+logic [WIDTH*FCL_OUTPUT_DIM-1:0] lfsr_out;
 // Instantiate a LFSR module
-LFSR #(.WIDTH(WIDTH*OUTPUT_DIM)) lfsr (
+LFSR #(.WIDTH(WIDTH*FCL_OUTPUT_DIM)) lfsr (
     .clk(clk),
     .rst(reset),
     .out(lfsr_out)
 );
 
-state_t state;
 typedef enum {INIT, RUN} state_t;
-logic [$clog2(INPUT_DIM)-1:0] j;
+state_t state;
+logic [$clog2(FCL_INPUT_DIM)-1:0] j;
 
 always_ff @(posedge clk or negedge reset) begin
     if (!reset) begin
-        state <= 0;
+        state <= INIT;
         j <= 0;
     end else begin
         if(state == INIT) begin
             // Initialize weights and biases to zero (or random later)
-            for(int i = 0; i < OUTPUT_DIM; i++) begin
-                weights[j][i] <= lfsr_out[i*WIDTH-1 +: WIDTH];
+            for(int i = 0; i < FCL_OUTPUT_DIM; i++) begin
+                fcl_input_weights[j][i] <= lfsr_out[i*WIDTH +: WIDTH];
             end
             j <= j + 1;
 
-            if(j == INPUT_DIM) begin
+            if(j == FCL_INPUT_DIM) begin
                 state <= RUN;
             end
         end
         else if(state == RUN) begin
             // update weights
             fcl_input_weights <= fcl_output_weights;
-
         end
     end
 end
-
-
-
 
 endmodule
