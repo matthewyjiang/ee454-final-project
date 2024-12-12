43e0d7ca789aab8d2440511770080b91b3495d1d
Matthew Jiang <jiangmy@usc.edu>
2024-12-11 17:51:28 -0800

    added ulil and test:

---
+++ util.sv
@@ -0,0 +1,24 @@
+package utilities;
+    virtual class util#(parameter WIDTH, FIXED_POINT_INDEX);
+        function logic [WIDTH-1:0] fixed_point_multiply (
+            input logic [WIDTH-1:0] a,
+            input logic [WIDTH-1:0] b
+        );
+
+            logic [2*WIDTH-1:0] product;
+            logic [WIDTH-1:0] result;
+            product = a * b;
+            product = product >> FIXED_POINT_INDEX;
+
+            product = product + (1 << (FIXED_POINT_INDEX-1));
+            product = product >> FIXED_POINT_INDEX;
+            
+            result = product;
+            return result;  
+        endfunction
+    endclass
+
+    
+endpackage
+
+// is there any case where we need to pad 0's to make sure it's exactly 32 bit ? << maybe only for inputs
