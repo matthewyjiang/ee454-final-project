d00115726a8bea200edc9265c90ab859f63c95d5
Claire Liang <caliang@usc.edu>
2024-12-12 23:12:59 -0800

    load label data and print accuracy

--- scripts/mnist_dataset.py
+++ scripts/mnist_dataset.py
@@ -41,14 +41,28 @@ print("Train Labels Shape:", train_labels.shape)
 print("Test Images Shape:", test_images.shape)
 print("Test Labels Shape:", test_labels.shape)
 
+num_train = 1
+
 # Save the first 100 images to a .mem file
 mem_file = "mnist_images.mem"
+mem_file = "image_data1.hex"
 with open(mem_file, "w") as f:
-    for i in range(5):
+    for i in range(num_train):
         verilog_lines = []
         for row in train_images[i] / 255.0:  # Normalize the 8-bit values to [0, 1]
-            row_data = "\n".join([f"{int(pixel * 256):04X}0000" for pixel in row])  # Scale and convert to hex
+            row_data = "\n".join([f"{int(pixel * 256):08X}0000" for pixel in row])  # Scale and convert to hex
             verilog_lines.append(row_data)
         f.write("\n".join(verilog_lines) + "\n")
+
+# Save labels to .mem file
+mem_file = "mnist_labels.mem"
+mem_file = "label_data1.hex"
+with open(mem_file, "w") as f:
+    for i in range(num_train):
+        label = train_labels[i]
+        one_hot = [0] * 10
+        one_hot[label] = 1
+        label = "\n".join([f"{bit:08X}0000" for bit in one_hot])
+        f.write(f"{label}\n")
 
 print(f"Converted first 100 images saved to {mem_file}")
